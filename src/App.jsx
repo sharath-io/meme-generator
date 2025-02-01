f8ae8e284829b18366d245d7fc8b33ac6a8b797b
Sharath <sharath.esr@gmail.com>
2025-02-01 21:38:54 +0530

    feat: add component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css'
+import "./App.css";
+import logo from "./assets/logo.png"
 
 function App() {
-
   return (
     <>
-      <h1>Meme Generator</h1>
+      <div className="heading">
+        <img src={logo} alt="logo"/>
+        <h1>Meme Generator</h1>
+      </div>
+
+      <div className="form">
+        <div className="input-section">
+
+        <label>TopText :
+          <input type="text" name="topText" placeholder="shutup"/>
+        </label>
+
+        <label>Bottom Text :
+          <input type="text" name="bottomText" placeholder="And take my money"/>
+        </label>
+        </div>
+
+        <button>Get a new Meme Image 🖼️ </button>
+      </div> 
+
+      <div className="meme">
+                <img src=  "https://i.imgflip.com/1bij.jpg"/>
+                <span className="top">Top Text</span>
+                <span className="bottom">Bottom Text</span>
+      </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
